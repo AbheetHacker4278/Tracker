7fdcf611a8ddc586f2cc5a17727415942332f712
Abheet seth <114290748+AbheetHacker4278@users.noreply.github.com>
2026-08-19 16:52:05 +0530

    Ignore config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// config.js - Supabase Environment Configuration
-window.SUPABASE_CONFIG = {
-  URL: "https://kbxvildpgsciywtjpwmb.supabase.co",
-  ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtieHZpbGRwZ3NjaXl3dGpwd21iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcxMjc4NzYsImV4cCI6MjEwMjcwMzg3Nn0.ooE2Q00ZL_OEmvQwyMd_NZlIbpyV3ggNCEuHI5BjEr0"
-};
